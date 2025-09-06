ab699d7d4a53710f9f84741b10392092e5457204
geekfrontend <geekfrontend16@gmail.com>
2025-09-06 17:00:43 +0800

    chore: test husky pre-commit

---
+++ .prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es6",
+  tabWidth: 2,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
--- src/provider.tsx
+++ src/provider.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { HeroUIProvider } from "@heroui/system";
 
 export function Provider({ children }: { children: React.ReactNode }) {
